2ca08164a2c947b25f9e5a9149474561defc3891
Andrew Olesky <oleskyinbox@gmail.com>
2025-06-21 13:43:13 -0400

    Set up server. Tested paths.

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
-/*
-Challenge: 
-1. Initialise a nodejs project:
-	Name: “from-the-other-side”.
-    Description: “A platform for sharing ghostly encounters”.
+import { createServer } from "node:http";
+import path from "node:path";
+import { pathToFileURL } from "node:url";
 
-2. Enable modular js (in package.json).
+const PORT = 8000;
 
-hint.md for help
-*/
+// console.log(import.meta.dirname);
+const __dirname = import.meta.dirname;
+// console.log(__dirname); // oldschool node, without "type": "module"
+
+// console.log("CWD", process.cwd());
+
+const server = createServer(async (req, res) => {
+  const absPathToResource = path.join(__dirname, "public", "index.html");
+  const relPathToResource = path.join("public", "index.html");
+  console.log("absolute: ", absPathToResource);
+  console.log("relative: ", relPathToResource);
+  //   res.statusCode = 200;
+  //   res.setHeader("Content-Type", "text/html");
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.end("<html><h1>The server is working</h1></html>");
+});
+
+server.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
