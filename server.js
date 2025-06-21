3a255369aeb5c77245939546c5e9e94d66d51f9a
Andrew Olesky <oleskyinbox@gmail.com>
2025-06-21 16:45:31 -0400

    Set up routing.

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 import { createServer } from "node:http";
 import path from "node:path";
 import { pathToFileURL } from "node:url";
+import { serveStatic } from "./utils/serveStatic.js";
+import fs from "node:fs/promises";
 
 const PORT = 8000;
 
@@ -11,14 +13,29 @@ const __dirname = import.meta.dirname;
 // console.log("CWD", process.cwd());
 
 const server = createServer(async (req, res) => {
-  const absPathToResource = path.join(__dirname, "public", "index.html");
-  const relPathToResource = path.join("public", "index.html");
-  console.log("absolute: ", absPathToResource);
-  console.log("relative: ", relPathToResource);
+  //   const absPathToResource = path.join(__dirname, "public", "index.html");
+  //   const relPathToResource = path.join("public", "index.html");
+  //   console.log("absolute: ", absPathToResource);
+  //   console.log("relative: ", relPathToResource);
+
+  //   const content = fs.readFileSync(serveStatic(__dirname), "utf8");
+
   //   res.statusCode = 200;
   //   res.setHeader("Content-Type", "text/html");
-  res.writeHead(200, { "Content-Type": "text/html" });
-  res.end("<html><h1>The server is working</h1></html>");
+  //   res.writeHead(200, { "Content-Type": "text/html" });
+  //   res.end("<html><h1>The server is working</h1></html>");
+  //   res.end(content);
+
+  //   fs.readFile(serveStatic(__dirname), "utf8", (err, content) => {
+  //     if (err) {
+  //       console.log(err);
+  //       return;
+  //     }
+  //     res.writeHead(200, { "Content-Type": "text/html" });
+  //     res.end(content);
+  //   });
+
+  await serveStatic(req, res, __dirname);
 });
 
 server.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
